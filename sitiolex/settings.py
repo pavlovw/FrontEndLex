5f786073cc162a8e335a895524f5caa0c59aaf31
pavlovw <133174113+pavlovw@users.noreply.github.com>
2024-12-15 20:37:09 +0000

    lol xd

--- sitiolex/settings.py
+++ sitiolex/settings.py
@@ -131,6 +131,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
